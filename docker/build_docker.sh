96a6b79d89b1732a13e36d941fcd5275a64cd417
Ryan Burda <rburda@squarespace.com>
2021-10-21 12:40:03 -0400

    Switched to multi-stage build to remove local artifacts

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -4,9 +4,6 @@ DOCKER_BASE="${PWD}/`dirname $0`"
 
 cd $DOCKER_BASE
 
-docker build .. -f Dockerfile.build -t dasit_build
-docker run --rm -v "${PWD}/../:/application_source" -u $(id -u):$(id -g) -ti dasit_build
-
 docker build .. -f Dockerfile.app -t dasit-app
 docker build .. -f Dockerfile.pg -t dasit-postgresql
 
