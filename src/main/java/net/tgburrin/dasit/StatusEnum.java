821f4f7638be448e3052d807774dc0c7af3aa1d5
Tim Burrington <tgburrin@users.noreply.github.com>
2022-06-28 21:52:52 -0400

    Adding back some of the string conversion methods.

--- src/main/java/net/tgburrin/dasit/StatusEnum.java
+++ src/main/java/net/tgburrin/dasit/StatusEnum.java
@@ -1,7 +1,19 @@
 package net.tgburrin.dasit;
 
 public enum StatusEnum {
-	ACTIVE,
-	INACTIVE
+	ACTIVE("A"),
+	INACTIVE("I")
 	;
+
+	private String s;
+
+	StatusEnum(String s) {
+		this.s = s;
+	}
+
+	public static StatusEnum getEnum(String value) {
+		for(StatusEnum es : values())
+			if(es.toString().equals(value)) return es;
+		throw new IllegalArgumentException();
+	}
 }
